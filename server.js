84879756bc2538ae4fe268fe9569c317885d38d2
asfarkhan07 <asfar.nfs2010@gmail.com>
2026-07-19 03:46:50 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ if(process.env.NODE_ENV==='production'){
     //Set static folder
     app.use(express.static('client/build'));
 
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    })
+    app.get('/:any(*)', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 }
 
 console.log('Routes Mounted')
